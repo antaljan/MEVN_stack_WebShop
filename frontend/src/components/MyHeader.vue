debe21a4ea8d2303f291b1f097f1fd70f90fb292
János Antali <139051150+antaljan@users.noreply.github.com>
2025-06-09 21:33:46 +0200

    new headder

--- frontend/src/components/MyHeader.vue
+++ frontend/src/components/MyHeader.vue
@@ -1,12 +1,12 @@
 <template>
-  <v-app>
-    <v-app-bar app color="primary" dark class="header">
-      <v-container class="d-flex align-center justify-space-between">
-        <v-avatar icon>
-          <v-img src="/MyLogo.png" max-height="40px"></v-img>
-        </v-avatar>
-        <span class="ml-3 text-h6">Edith Skills Lab</span>
-        <v-menu open-on-hover>
+  <v-app-bar
+    color= #2196F3
+    elevation="14"
+  >
+      <v-app-bar-nav-icon>
+      </v-app-bar-nav-icon>
+
+      <v-menu open-on-hover>
           <template v-slot:activator="{ props }">
             <v-btn v-bind="props" text>
               Menü
@@ -17,35 +17,24 @@
               <v-list-item-title>{{ item.name }}</v-list-item-title>
             </v-list-item>
           </v-list>
-        </v-menu>
-        <v-btn text>Kapcsolat</v-btn>
-        <v-btn color="orange" dark>
+      </v-menu>
+
+      <v-toolbar-title>Edith Skills Lab</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn color="white" dark>
           Bejelentkezés
-        </v-btn>
-      </v-container>
-    </v-app-bar>
-  </v-app>
+      </v-btn>
+
+  </v-app-bar>
 </template>
 
 <script setup>
-import { ref } from "vue";
-
-const menuItems = ref([
-  { name: "Blog" },
-  { name: "Oktatás" },
-  { name: "Coaching" },
-]);
+  import { ref } from "vue";
+  const menuItems = ref([
+    { name: "Blog" },
+    { name: "Oktatás" },
+    { name: "Coaching" },
+  ]);
 </script>
-
-<style scoped>
-.header{
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  z-index: 1000;
-}
-.v-app-bar {
-  padding: 0 20px;
-}
-</style>
