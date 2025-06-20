62f01b0ea254793bd7c1ec3816c106da43b08f1d
János Antali <139051150+antaljan@users.noreply.github.com>
2025-06-20 23:43:10 +0200

    add login functionality to header

--- frontend/src/components/MyHeader.vue
+++ frontend/src/components/MyHeader.vue
@@ -1,24 +1,80 @@
 <template>
-  <!-- Navbar (sit on top) -->
+<!-- Navbar (sit on top) -->
   <div class="w3-top">
     <div class="w3-bar" id="myNavbar">
-      <a class="w3-bar-item w3-button w3-hover-black w3-hide-medium w3-hide-large w3-right" href="javascript:void(0);" @click="toggleFunction" title="Toggle Navigation Menu">
+      <a class="w3-bar-item w3-button w3-hover-black w3-hide-medium w3-hide-large w3-left" href="javascript:void(0);" @click="toggleFunction" title="Toggle Navigation Menu">
         <i class="fa fa-bars"></i>
       </a>
-      <a href="/#home" class="w3-bar-item w3-button">{{ menuButtonHome[selectedLanguage] }}</a>
+      <a href="/#home" class="w3-bar-item w3-button w3-hide-small">{{ menuButtonHome[selectedLanguage] }}</a>
       <a href="/#about" class="w3-bar-item w3-button w3-hide-small"> {{menuButtonAbout[selectedLanguage]}}</a>
       <a href="/#contact" class="w3-bar-item w3-button w3-hide-small"> {{ menuButtonContact[selectedLanguage] }}</a>
-      <a href="/#" class="w3-bar-item w3-button w3-hide-small w3-right w3-hover-red">
-        <i class="fa fa-user"></i>
-      </a>
+<!-- login -->
+      <v-container class="w3-bar-item w3-button w3-right">
+      <p  @click="dialog = true">
+        <v-icon name="loginIcon" color="black">mdi-login</v-icon>
+      </p>
+      <v-dialog v-model="dialog" max-width="500">
+        <v-card>
+        <v-card-title class="headline">
+          {{ isLogin ? 'Login' : 'Registrieren' }}
+        </v-card-title>
+        <v-card-text>
+          <v-form ref="form" v-model="valid">
+            <v-text-field
+              v-if="!isLogin"
+              v-model="name"
+              label="Name"
+              :rules="[v => !!v || 'Name ist erforderlich']"
+              required
+            ></v-text-field>
+            <v-text-field
+              v-model="email"
+              label="E-Mail"
+              :rules="[v => /.+@.+\..+/.test(v) || 'Gültige E-Mail angeben']"
+              required
+            ></v-text-field>
+            <v-text-field
+              v-model="password"
+              label="Passwort"
+              :rules="[v => !!v || 'Passwort ist erforderlich']"
+              type="password"
+              required
+            ></v-text-field>
+            <v-checkbox
+              v-if="!isLogin"
+              v-model="gdpr"
+              :rules="[v => !!v || 'Bitte stimme der Datenschutzerklärung zu']"
+              label="Ich stimme der Datenschutzerklärung zu"
+              required
+            ></v-checkbox>
+          </v-form>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="green" text @click="submit">
+            {{ isLogin ? 'Einloggen' : 'Registrieren' }}
+          </v-btn>
+          <v-btn color="grey" text @click="dialog = false">Abbrechen</v-btn>
+        </v-card-actions>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn text @click="toggleForm">
+            {{ isLogin ? 'Noch kein Konto? Jetzt registrieren' : 'Schon registriert? Jetzt einloggen' }}
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-container>
+<!-- Language selection -->
       <select id="langselect" class="w3-bar-item w3-button w3-right" @change="changeLanguage">
         <option value="en">EN</option>
         <option value="hu" selected>HU</option>
         <option value="de">DE</option>
       </select>
     </div>
-    <!-- Navbar on small screens -->
+<!-- Navbar on small screens -->
     <div id="navDemo" class="w3-bar-block w3-white w3-hide w3-hide-large w3-hide-medium" @change="changeLanguage">
+      <a href="/#home" class="w3-bar-item w3-button" @click="toggleFunction"> {{menuButtonHome[selectedLanguage]}}</a>
       <a href="/#about" class="w3-bar-item w3-button" @click="toggleFunction"> {{menuButtonAbout[selectedLanguage]}}</a>
       <a href="/#contact" class="w3-bar-item w3-button" @click="toggleFunction">{{ menuButtonContact[selectedLanguage] }}</a>
     </div>
@@ -73,4 +129,45 @@ function changeLanguage(event) {
   document.documentElement.lang = selectedLanguage.value;
 }
 </script>
-
+<script>
+export default {
+  data() {
+    return {
+      dialog: false,
+      valid: false,
+      isLogin: true,
+      name: '',
+      email: '',
+      password: '',
+      gdpr: false
+    }
+  },
+  methods: {
+    toggleForm() {
+      this.isLogin = !this.isLogin
+      this.$refs.form.resetValidation()
+    },
+    submit() {
+      if (this.$refs.form.validate()) {
+        if (this.isLogin) {
+          console.log('Login:', this.email, this.password)
+        } else {
+          console.log('Registrierung:', this.name, this.email, this.password, 'GDPR:', this.gdpr)
+        }
+        this.dialog = false
+        this.name = ''
+        this.email = ''
+        this.password = ''
+        this.gdpr = false
+      }
+    }
+  }
+}
+</script>
+<style>
+.hover-glow:hover {
+  box-shadow: 0 0 12px rgba(0, 123, 255, 0.7);
+  transform: scale(1.1);
+  transition: all 0.3s ease;
+}
+</style>
