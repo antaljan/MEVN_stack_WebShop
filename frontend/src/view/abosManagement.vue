546d2fedb8f1b5f3175e887df8ee0d31467ba001
János Antali <139051150+antaljan@users.noreply.github.com>
2025-08-01 19:05:14 +0200

    add v-card for subscriber count and weather icon

--- frontend/src/view/abosManagement.vue
+++ frontend/src/view/abosManagement.vue
@@ -1,16 +1,51 @@
 <template>
   <my-header/>
   <v-app>
-    <v-card>
-      <v-card-title class="text-h5">Abonnenten Management</v-card-title>
-      <v-card-text>
-        <p>Aktuelle Abonnentenzahl: {{ subscriberCount }}</p>
-        <p>Hier können Sie Ihre Abonnenten verwalten.</p>
-      </v-card-text>
-      <v-card-actions>
-        <v-btn color="primary" @click="$router.push('/abos-management/add')">Neuen Abonnenten hinzufügen</v-btn>
-      </v-card-actions>
-    </v-card>
+    <v-card
+    class="mx-auto"
+    max-width="368"
+  >
+    <v-card-item title="Hírlevél feliratkozás"></v-card-item>
+    <v-card-text class="py-0">
+      <v-row align="center" no-gutters>
+        <v-col
+          class="text-h2"
+          cols="6"
+        >
+          {{ subscriberCount }}
+        </v-col>
+      </v-row>
+    </v-card-text>
+    <div class="d-flex py-3 justify-space-between">
+      <v-list-item
+        density="compact"
+        prepend-icon="mdi-share-variant"
+      >
+        <v-list-item-subtitle>feliratkozó</v-list-item-subtitle>
+      </v-list-item>
+    </div>
+    <v-expand-transition>
+      <div v-if="expand">
+        <v-list class="bg-transparent">
+          <v-list-item
+            v-for="item in aboList"
+            :key="item.id"
+            :name="item.name"
+            :firstname="item.firstname"
+            :email="item.email"
+          >
+          </v-list-item>
+        </v-list>
+      </div>
+    </v-expand-transition>
+    <v-divider></v-divider>
+    <v-card-actions>
+      <v-btn
+        :text="!expand ? 'Részletek' : 'kevesebb részlet'"
+        @click="expand = !expand"
+      ></v-btn>
+    </v-card-actions>
+  </v-card>
   </v-app>
   <my-footer/>
 </template>
@@ -31,4 +66,19 @@
       console.error('Fehler beim Laden der Abonnenten:', error)
     }
   })
+  // get abonements from the API
+  const abonements = ref([]);
+  onMounted(async () => {
+    try {
+      const response = await axios.get('https://yowayoli.com/api/abonements')
+      abonements.value = response.data;
+    } catch (error) {
+      console.error('Fehler beim Laden der Abonnements:', error);
+    }
+  });
+  const aboList = [
+    abonements
+  ]
+
+  const expand = ref(false)
 </script>
