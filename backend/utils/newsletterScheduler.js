3eba2b39c257eaf32ca537d2d3912a05004263ef
János Antali <139051150+antaljan@users.noreply.github.com>
2025-08-07 21:57:45 +0200

    bugfix: newsletterschedulung

--- backend/utils/newsletterScheduler.js
+++ backend/utils/newsletterScheduler.js
@@ -15,30 +15,32 @@ function startNewsletterScheduler() {
   cron.schedule('0 */1 * * *', async () => {
     const now = new Date();
     console.log(`[CRON] Checking for newsletters to send at ${now.toISOString()}`);
-
     try {
         const newsletters = await newsletterModel.getDueNewsletters(now);
-
+        console.log(`[CRON} number of newsletters to send:"${newsletters.length}"`);
         for (const newsletter of newsletters) {
             const subscribers = newsletter.subscribers;
-
+            console.log(`[CRON] "${newsletter.subject}"The newsletter has to send for "${subscribers.length}" number of subscribers.`);
             for (const subscriber of subscribers) {
                 const content = newsletterController.fillTemplate(newsletter.rawcontent, {
                     firstname: subscriber.firstname,
                     name: subscriber.name,
                     email: subscriber.email
                 });
-                await emailService.sendHtml(subscriber.email, newsletter.subject, content);
-            }
-
-        await newsletterModel.markAsSent(newsletter._id);
-        console.log(`[CRON] Newsletter "${newsletter.subject}" sent and marked as sent.`);
-        }
+                console.log(`The subscriber: "${subscriber.email}" will get the newsletter:"${newsletter.subject}".`);
+                try{
+                    await emailService.sendHtml(subscriber.email, newsletter.subject, content);
+                } catch (err) {
+                    console.error('[CRON] Error during newsletter dispatch:', err);
+                }
+            };
+            await newsletterModel.markAsSent(newsletter._id);
+            console.log(`[CRON] Newsletter "${newsletter.subject}" sent and marked as sent.`);
+        };
     } catch (err) {
-        console.error('[CRON] Error during newsletter dispatch:', err);
+        console.error('[CRON] Error during get due newsletters:', err);
     }
-  });
-}
+})};
 
 module.exports = {
     startNewsletterScheduler
